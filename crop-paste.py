2826096d42f6e221eaa48daa8022fe6a2d302daf
guoqiushi <guoqiushi910@gmail>
2024-06-12 18:01:53 +0800

    rewrite copy-paste

--- crop-paste.py
+++ crop-paste.py
@@ -6,7 +6,7 @@ import time
 from PIL import Image
 import random
 import torch
-import yaml
+import glob
 import cv2
 import numpy as np
 
@@ -18,17 +18,79 @@ yolo_obj365 = torch.hub.load('ultralytics/yolov5', 'custom', path)
 sam = sam_model_registry['vit_b'](checkpoint='sam_vit_b_01ec64.pth')
 sam.to('cuda')
 predictor = SamPredictor(sam)
+
+
 class contextAwareTool:
     def __init__(self):
         self.blip_model = blip_model
         self.bert_model = bert_model
         self.yolo = yolo_obj365
-        self.obj365_labels = self._get_obj365_labels()
+        # self.obj365_labels = self._get_obj365_labels()
+
+    def copy_paste(self,base_image_path):
+        category =self._get_most_relevent_category(base_image_path)
+        print(category)
+        target_img_list = glob.glob('D:/dataset/obj365/{}/*.png'.format(category)) + glob.glob(
+            'D:/dataset/obj365/{}/*.jpg'.format(category))
+        target_img_path = random.choice(target_img_list)
+        target_mask, bbox = tool._get_segmentation_given_category(target_img_path)
+        x1, y1, x2, y2 = bbox
+        target_img = cv2.imread(target_img_path)
+        base_img = cv2.imread(base_image_path)
+        h_t, w_t, _ = target_img.shape
+        upper_left = [100, 100]
+        for i in range(y1, y2):
+            for j in range(x1, x2):
+                if target_mask[i, j] == 1:
+                    base_img[upper_left[1] + i, upper_left[0] + j] = target_img[i, j]
+        return base_img
+
+
+    def _get_segmentation_given_category(self, img_path):
+        x1, y1, x2, y2 = self._get_bbox_given(img_path)
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        input_box = np.array([x1, y1, x2, y2])
+        predictor.set_image(img)
+        mask, scores, logits = predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_box[None, :],
+            multimask_output=False,
+        )
+        res = np.full_like(mask, fill_value=1, dtype=np.int16)
+        res = res * mask
+        res = np.transpose(res, (1, 2, 0))
+        return res, [x1, y1, x2, y2]
+
+
+    def _get_bbox_given(self, img_path):
+        img = cv2.imread(img_path)
+        res = self.yolo(img_path)
+        res = res.pandas().xyxy[0]
+        # target_df = res[res['name']==category]
+        target_df = res[res['confidence'] > 0.9]
+        if len(target_df) == 0:
+            target_df = res
+        # index = random.randint(0,len(target_df))
+        # return target_df
+        return [int(x) for x in target_df.iloc[0, :].tolist()[:4]]
+
+
+    def _get_most_relevent_category(self, img_path):
+        caption = self._get_img_caption(img_path)
+        categories = os.listdir('D:/dataset/obj365/')
+        category_score = {}
+        for category in categories:
+            category_score[category] = self._get_similarity(category, caption)
+        return sorted(category_score.items(), key=lambda x: x[1], reverse=True)[0][0]
+
 
     def _get_similarity(self, text_1, text_2):
         sentence_embeddings = self.bert_model.encode([text_1, text_2])
         similarity_scores = cosine_similarity([sentence_embeddings[0]], [sentence_embeddings[1]])
         return similarity_scores[0][0]
+
 
     def _get_img_caption(self, img_path):
         raw_image = Image.open(img_path).convert('RGB')
@@ -37,27 +99,7 @@ class contextAwareTool:
         out = self.blip_model.generate(**inputs)
         return processor.decode(out[0], skip_special_tokens=True)
 
-    def _get_detection_obj365(self, img_path):
-        img = cv2.imread(img_path)
-        res = self.yolo(img)
-        return res
 
-    def _get_obj365_labels(self):
-        with open('Objects365.yaml', 'r', encoding='utf-8') as file:
-            labels = yaml.load(file, Loader=yaml.FullLoader)
-        obj365_labels = list(labels['names'].values())
-        return obj365_labels
-
-    def _get_most_relevent_object_bbox(self, base_img, obj_img):
-        similarity_res = []
-        obj_res = self._get_detection_obj365(obj_img).pandas().xyxy[0]
-        base_img_caption = self._get_img_caption(base_img)
-        for i in range(len(obj_res)):
-            similarity_res.append([i, self._get_similarity(base_img_caption, obj_res.iloc[i, -1])])
-        index = sorted(similarity_res, key=lambda x: x[1], reverse=True)[0][0]
-        # print(index)
-        coor = [int(x) for x in obj_res.iloc[index].tolist()[:4]]
-        return coor
 
     def _get_position(self, base_img):
         detect_res = self._get_detection_obj365(base_img)
@@ -73,26 +115,6 @@ class contextAwareTool:
                 if x < int(item[0]) or x > int(item[2]) or y < int(item[1]) or y > int(item[3]):
                     return (x, y)
 
-    def _get_segmentation_mask(self, obj_img_path, box):
-        img = cv2.imread(obj_img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        input_box = np.array(box)
-        predictor.set_image(img)
-        mask, scores, logits = predictor.predict(
-            point_coords=None,
-            point_labels=None,
-            box=input_box[None, :],
-            multimask_output=False,
-        )
-        res = np.full_like(mask, fill_value=1, dtype=np.int16)
-        res = res * mask
-        res = np.transpose(res, (1, 2, 0))
-        return res
 
-    def copy_paste(self, base_img, obj_img):
-        left_upper = self._get_position(base_img)
-        obj_bbox = self._get_most_relevent_object_bbox(base_img, obj_img)
-        obj_mask = self._get_segmentation_mask(obj_img, obj_bbox)
-        h_obj, w_obj, _ = obj_mask.shape
 
-        return obj_mask
+
